7dcf14ca1434ae865760a35eb38b3ce05d48e928
adarshrajs1999 <153977458+adarshrajs1999@users.noreply.github.com>
2024-06-12 17:40:33 +0530

    first commit

---
+++ job_portal_app/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+# Create your models here.
+
+class User_model(AbstractUser):
+    is_employer = models.BooleanField(default = False)
+    is_jobseeker = models.BooleanField(default = False)
+
+class employer(models.Model):
+    user = models.ForeignKey(User_model, on_delete = models.CASCADE, related_name = 'employer_user')
+    admin_approval_status = models.BooleanField(default = 0)
+    name = models.CharField(max_length = 250)
+    phone_number = models.CharField(max_length = 12)
+    email = models.EmailField()
+    profile_picture = models.FileField()
+
+    def __str__(self):
+        return self.name
+
